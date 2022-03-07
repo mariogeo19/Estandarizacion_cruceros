6e0a4072b47d05ed6d39b480709f49fbe1e1409d
Mario Vasquez <mariogeo190192@gmail.com>
2022-03-07 15:35:04 -0600

    Se agrego codigo para limpiar datos de ctd de bajada

--- Union_datos_CTD.R
+++ Union_datos_CTD.R
@@ -3,21 +3,22 @@
 #archivo <- readLines(file("oceanografia/JCFINP2110_ CTD FINAL/JCFINP2110004_E2.TXT"))
 library(tidyverse)
 ##### Atomatizar la lectura de los archivos #####
-ctd_txt_data <- list.files(path = "oceanografia/JCFINP2110_ CTD FINAL/", pattern = ".TXT", full.names = TRUE, recursive = TRUE)
+ctd_txt_data <- list.files(path = "oceanografia/ctd/txt", pattern = ".txt", full.names = TRUE, recursive = TRUE)
+ctd_txt_data <- ctd_txt_data[2:length(ctd_txt_data)]
 read_ctd <- function(x){read.table(x, skip = 34)}
 
 extract_asterisk <- function(x){
-    stringr::str_replace(x, '\\*', '') %>% 
+  stringr::str_replace_all(x, "\\*", " ") %>% 
     as.numeric()}
 
 read_archivo_station <- function(y){a = readLines(file(y)); as.numeric(a <- substr(a[3],15,20))}
 read_archivo_cruise <- function(y){a = readLines(file(y)); substr(a[6], 15,24)}
-read_archivo_year <- function(y){a = readLines(file(y)); as.numeric(substr(a[27], 22,25))}
-read_archivo_month <- function(y){a = readLines(file(y)); substr(a[27], 19,20)}
-read_archivo_day <- function(y){a = readLines(file(y)); substr(a[27], 16,17)}
-read_archivo_hour <- function(y){a = readLines(file(y)); substr(a[27], 27,28)}
-read_archivo_minute <- function(y){a = readLines(file(y)); substr(a[27], 30,31)}
-read_archivo_second <- function(y){a = readLines(file(y)); substr(a[27], 33,34)}
+read_archivo_year <- function(y){a = readLines(file(y)); as.numeric(substr(a[28], 22,25))}
+read_archivo_month <- function(y){a = readLines(file(y)); substr(a[28], 19,20)}
+read_archivo_day <- function(y){a = readLines(file(y)); substr(a[28], 16,17)}
+read_archivo_hour <- function(y){a = readLines(file(y)); substr(a[28], 27,28)}
+read_archivo_minute <- function(y){a = readLines(file(y)); substr(a[28], 30,31)}
+read_archivo_second <- function(y){a = readLines(file(y)); substr(a[28], 33,34)}
 
 metadato_ctd_station <- purrr::map(ctd_txt_data, .f = read_archivo_station)
 metadato_ctd_cruise <- purrr::map(ctd_txt_data, .f = read_archivo_cruise)
@@ -40,7 +41,7 @@ data_ctd <- purrr::map(ctd_txt_data, .f = read_ctd) %>%
   purrr::reduce(rbind)
 
 name_variables_ctd <- c("Latitude", "Longitude", "Pressure", "Depth","Temperature","Conductivity",
-                        "Salinity","SigmaT","Optical_ppm","Optical","ECO-SCATT","Chla","pH",
+                        "Salinity","SigmaT","Optical_ppm","Optical","Sound_Velocity ","ECO-SCATT","Chla","pH",
                         "PAR", "PAR-Ref","Eh","TDS","Transmissometer", "Cruise","Station","Year",
                         "Month","Day","Hour","Minute","Second")
 
@@ -48,8 +49,9 @@ colnames(data_ctd) <- name_variables_ctd
 
 data_ctd <- data_ctd %>% 
   dplyr::rename(Oxygen = Optical_ppm, Oxigen_percent = Optical, ECO = `ECO-SCATT`, `ECO-Chla` = Chla) %>% 
-  dplyr::select(Cruise:Second, Latitude:SigmaT, Oxygen:`ECO-Chla`, pH:Transmissometer) %>% 
-  dplyr::filter(Latitude >0)
+  dplyr::select(Cruise:Second, Latitude:SigmaT, Oxygen:`ECO-Chla`, pH:Transmissometer)
+
+# ELIMINAR ASTERISTICOS Y DATOS CON POSICION GEOGRAFICA -------------------
 
 datos_sin_asteriscos <- data_ctd %>% 
   dplyr::select(9:ncol(data_ctd)) %>% 
@@ -58,8 +60,37 @@ datos_sin_asteriscos <- data_ctd %>%
 datos_fecha <- data_ctd %>% 
   dplyr::select(1:8)
 
-data_ctd <- bind_cols(datos_fecha, datos_sin_asteriscos)
-cruise <- data_ctd$Cruise[1]
-write.csv(data_ctd, paste0("oceanografia/",cruise,"_CTD_completo.csv"), row.names = FALSE)
-plot(data_ctd$Longitude, data_ctd$Latitude)
-  
+data_ctd <- bind_cols(datos_fecha, datos_sin_asteriscos) %>% 
+  dplyr::filter(Latitude >0, pH > 1)
+
+
+# OBTENER DATOS DE BAJADA DEL CTD -----------------------------------------
+
+ctd_bajada <- function(data_ctd_completo, station){
+  eo <- data_ctd_completo %>% 
+    dplyr::filter(Station == station)
+  num_nrow <- nrow(eo)
+  eo <- eo %>% 
+    mutate(fid = 1:num_nrow)
+  eo_max <- max(eo$Pressure)
+  corte <- eo %>% 
+    filter(Pressure == eo_max)
+  corte_b <- corte$fid[1]
+  eo_bajada <- eo %>% 
+    filter(fid <= corte_b) %>% 
+    dplyr::select(-fid)
+}
+
+estaciones_bajada <- unique(data_ctd$Station)
+
+df_eo <- list()
+for (i in estaciones_bajada) {
+  df <- ctd_bajada(data_ctd, i)
+  df_eo <- rbind(df_eo, df)
+}
+
+# GUARDAR ARCHIVO LIMPIO --------------------------------------------------
+
+cruise <- df_eo$Cruise[1]
+plot(df_eo$Longitude, df_eo$Latitude)
+write.csv(df_eo, paste0("oceanografia/",cruise,"_CTD_completo.csv"), row.names = FALSE)
